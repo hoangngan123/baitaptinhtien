d21a98eaacf422ec69735f48e891e458f30b2f74
hoangngan123 <147030773+hoangngan123@users.noreply.github.com>
2023-10-12 07:22:27 +0000

    x

--- s00_bailam.py
+++ s00_bailam.py
@@ -31,5 +31,8 @@ tongtienmuahang(None)                   | None
 
 #region bailam
 def tongtienmuahang(gia_truoc_thue):
-  return 'todo'
+
+  gia_sau_thue = gia_truoc_thue*1.1
+  gia_sau_thue=int(gia_sau_thue)
+  return gia_sau_thue
 #endregion bailam
